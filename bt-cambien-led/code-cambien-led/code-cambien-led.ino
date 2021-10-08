880532c738680773d2cd03007bb92455bdd4c563
Nguyen Hong Dan <91358640+dannguyen2102@users.noreply.github.com>
2021-10-08 23:39:49 +0700

    update and bug fixes
    sua loi cam bien va daang bai khoang cach

--- bt-cambien-led/code-cambien-led/code-cambien-led.ino
+++ bt-cambien-led/code-cambien-led/code-cambien-led.ino
@@ -1,12 +1,14 @@
 void setup ()
 {
-  Serial.begin (96000);
+  pinMode(9,OUTPUT);
+  Serial.begin (9600);
 }
 
 void loop()
 {
   int giatri = analogRead (A0);
-    int nhietdo=map (giatri,20,358,-40,125);
-    if (nhietdo>37) digitalWrite (9, HIGH);
+    int t =map (giatri,20,350,-40,125);
+     Serial.println(t);
+    if (t>37) digitalWrite (9, HIGH);
     else digitalWrite (9, LOW);
 }
